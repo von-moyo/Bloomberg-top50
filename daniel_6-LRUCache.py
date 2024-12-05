c565167d2f79e471369764d4ed7452015b336f2c
Kelvin <kelvinofili1@gmail.com>
2024-12-05 13:24:53 +0100

    more

--- daniel_6-LRUCache.py
+++ daniel_6-LRUCache.py
@@ -39,6 +39,25 @@ class LRUCache:
             self.remove(lru)
             del self.cache[lru.key]
         
+from collections import OrderedDict
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+    
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+        
+    
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            self.cache.popItem(last=False)
 
 
 # Your LRUCache object will be instantiated and called as such:
